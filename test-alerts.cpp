446ca8798c59004e89506ddea261d461fe5347f8
Raviteja-Singampalli <136675244+Raviteja-Singampalli@users.noreply.github.com>
2023-07-03 17:27:50 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -8,5 +8,8 @@ TEST_CASE("infers the breach according to limits") {
 }
 
 TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(25, 20, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+}
+TEST_CASE("infers the breach according to limits") {
+  REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
 }
